41d9d24d21487f2ce8d1f1396c5e3ff341d51ba9
Hans Bergren <hansbergren@gmail.com>
2020-03-05 15:21:01 -0500

    Add organization column to Account table (#22)
    PayID has a private API that will need API key authentication to access, which allows CRUD operations of users in the PayID service.
    
    Since multiple organizations could use the PayID service, we need some way to keep track of which organization is allowed to modify which users.
    
    This PR adds an organization column, which will hold a human-readable name of the organization that owns a given payment_pointer.
    
    TBD is where the API key authentication maps to a human readable name that gets supplied here.

--- src/db/schema/account.sql
+++ src/db/schema/account.sql
@@ -4,6 +4,7 @@
 CREATE TABLE IF NOT EXISTS account (
 	id uuid PRIMARY KEY DEFAULT(gen_random_uuid()),
 	payment_pointer varchar(200) UNIQUE NOT NULL,
+	organization varchar(50) NOT NULL,
 
 	-- AUDIT COLUMNS
 	created_at timestamp with time zone NOT NULL DEFAULT(CURRENT_TIMESTAMP),
@@ -11,6 +12,7 @@ CREATE TABLE IF NOT EXISTS account (
 
 	-- CONSTRAINTS
 	CONSTRAINT payment_pointer_length_nonzero CHECK(length(payment_pointer) > 0),
+	CONSTRAINT organization_length_nonzero CHECK(length(organization) > 0),
 
 	-- This is a black magic URL regex from https://mathiasbynens.be/demo/url-regex
 	-- It is an adaptation of the 'stephenhay' implementation, which was the shortest URL regex I could find.
