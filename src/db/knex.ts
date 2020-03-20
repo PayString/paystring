d95a656475567fa456d01138a4a02bc317fb8ada
Hans Bergren <hansbergren@gmail.com>
2020-03-20 14:49:13 -0400

    Add DB set statements (#107)
    I wanted some assurances about our database connections. In particular:
    
    - The timezone should always be UTC
      - This should really be set at the database configuration level, but in case the database is misconfigured, this ensures the created_at and updated_at fields are always in UTC regardless.
    - Lowers the statement timeout from 10 seconds to 3 seconds. None of our queries are complicated and we only have 2 tables. Even 3 seconds seems ridiculously high.

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -5,6 +5,20 @@ import databaseConfiguration from './databaseConfiguration'
 const knex = knexInit({
   client: 'pg',
   connection: databaseConfiguration,
+  pool: {
+    /* eslint-disable */
+    afterCreate(conn: any, done: Function): void {
+      conn.query('SET timezone="UTC";', (err: Error) => {
+        if (err) return done(err, conn)
+
+        conn.query('SET statement_timeout TO 3000;', (err: Error) => {
+          // if err is not falsy, connection is discarded from pool
+          done(err, conn)
+        })
+      })
+    },
+    /* eslint-enable */
+  },
 })
 
 export default knex
