01ca3146701dcb0c774d5a5d5dcb479306fc0da3
Elliot Lee <github.public@intelliot.com>
2020-03-25 09:48:41 -0700

    run_payid_demo.sh: Fix typos (#150)
    Just fixed a couple of typos in run_payid_demo.sh

--- demo/run_payid_demo.sh
+++ demo/run_payid_demo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo -e "\n\nPlease enter a short, single word alphanumberic name that will be used as the subdomain for your PayID demo server: "
+echo -e "\n\nPlease enter a short, single-word alphanumeric name to use as the subdomain for your PayID demo server: "
 read ngrok_subdomain
 
 set +e
@@ -19,7 +19,7 @@ cd ${demo_dir}/../
 docker build -f demo/Dockerfile -t payid_demo:latest .
 docker run --env NGROK_SUBDOMAIN=${ngrok_subdomain} --name payid -d -p 8080:8080 -p 8081:8081 payid_demo:latest
 
-echo -e "\n\nSample curl commands to run  against your PayID demo server:"
+echo -e "\n\nSample curl commands to run against your PayID demo server:"
 
 echo -en """\n\ncurl --location --request POST http://localhost:8081/v1/users \
 --header 'Content-Type: application/json' \
@@ -41,6 +41,6 @@ echo "
 
 echo """curl --location --request GET https://${ngrok_subdomain}.ng.xpring.dev/${ngrok_subdomain} --header 'Accept: application/xrpl-testnet+json'"""
 
-echo -e "\n\n\nPayID is now availabe on localhost:8080 and localhost:8081\nYou can stop the demo by running 'docker stop -t 1 payid'\n\n"
+echo -e "\n\n\nPayID is now available on localhost:8080 and localhost:8081\nYou can stop the demo by running 'docker stop -t 1 payid'\n\n"
 
 echo -e "You can access the public API of your PayID server over the internet at https://${ngrok_subdomain}.ng.xpring.dev\n"
