1830d6a224524b7d4a75fdfd373d4f3043259d3c
Austin King <aking@ripple.com>
2020-06-02 09:15:14 -0700

    refactor: move intended hooks into explicit hooks directory (#429)
    * refactor: move intended hooks into explicit hooks directory
    
    * fix: lint

--- src/middlewares/payIds.ts
+++ src/middlewares/payIds.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 import getAllAddressInfoFromDatabase from '../data-access/payIds'
+import createMemo from '../hooks/memo'
 import {
   formatPaymentInfo,
   getPreferredAddressHeaderPair,
@@ -10,7 +11,6 @@ import { recordPayIdLookupResult } from '../services/metrics'
 import { urlToPayId, constructUrl } from '../services/urls'
 import HttpStatus from '../types/httpStatus'
 import { handleHttpError, LookupError, LookupErrorType } from '../utils/errors'
-import createMemo from '../utils/memo'
 
 /**
  * Resolves inbound requests to a PayID to their respective ledger addresses or other payment information.
