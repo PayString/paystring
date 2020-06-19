4e9b45f904e6f59b36a1a6095f9a0379f3496a1b
mouradski <mouradski@live.com>
2020-06-19 21:54:09 +0200

    Custom PostgreSql port via DB_PORT environment variable

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,7 @@ const config = {
   database: {
     connection: {
       host: process.env.DB_HOSTNAME ?? '127.0.0.1',
+      port: Number(process.env.DB_PORT) ??  5432,
       user: process.env.DB_USERNAME ?? 'postgres',
       password: process.env.DB_PASSWORD ?? 'password',
       database: process.env.DB_NAME ?? 'database_development',
