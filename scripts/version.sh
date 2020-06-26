57efb8d13bcbf045a2486f1cea2a594d6e52067c
Dino Rodriguez <dinoorodriguez@gmail.com>
2020-06-26 16:33:03 -0700

    feat: initial bump script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+# Bumps the NPM version, create a git tag, & bump the Docker version.
+# ! Expects all versions to be equal.
+#
+# $1 - The current NPM version.
+# $2 - The type of version bump (major, minor, patch).
+function bump() {
+   declare -r current_version=$1
+   declare -r bump_increment=$2
+   echo $bump_increment
+
+   if [[ $bump_increment != "major" && \
+         $bump_increment != "minor" && \
+         $bump_increment != "patch"
+   ]]; then
+      error "Invalid bump increment. Please specify 'major', 'minor', or 'patch'."
+   else
+      # Bump NPM version and git tag
+      declare -r new_version=$(npm version $bump_increment)
+      echo new_version
+
+      # "Bump" the Docker image version
+      # (Find and replace the old version for the new)
+      sed -e "s|payid:$current_version|payid:$new_version|g" < docker-compose.yml
+   fi
+}
+
 # Compares the Docker image version, NPM version, and Git tag version
 # in the repo. Throws an error if any do not match.
 #
