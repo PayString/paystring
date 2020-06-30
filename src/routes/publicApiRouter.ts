62e100bb70b455d2bb8a18cdb48e4bc00413480b
Hans Bergren <hansbergren@gmail.com>
2020-06-30 16:49:48 -0400

    Fix

---
+++ src/routes/publicApiRouter.ts
@@ -0,0 +1,54 @@
+import * as path from 'path'
+
+import * as express from 'express'
+
+import checkPublicApiVersionHeaders from '../middlewares/checkPublicApiVersionHeaders'
+import errorHandler, { wrapAsync } from '../middlewares/errorHandler'
+import initializeMetrics from '../middlewares/initializeMetrics'
+import getPaymentInfo from '../middlewares/payIds'
+import sendSuccess from '../middlewares/sendSuccess'
+
+const publicApiRouter = express.Router()
+
+/**
+ * Routes for the PayID Public API.
+ */
+publicApiRouter
+  // Allow the PayID Protocol to basically ignore CORS
+  .use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'PayID-Version')
+    res.header(
+      'Access-Control-Expose-Headers',
+      'PayID-Version, PayID-Server-Version',
+    )
+    next()
+  })
+
+  // Welcome page route
+  .get('/', (_req: express.Request, res: express.Response) => {
+    res.sendFile(path.join(__dirname, '../html/index.html'))
+  })
+
+  // Favicon route
+  .get('/favicon.ico', (_req: express.Request, res: express.Response) => {
+    res.sendFile(path.join(__dirname, '../html/favicon.ico'))
+  })
+
+  // Health route
+  .get('/status/health', sendSuccess)
+
+  // Base PayID route
+  .get(
+    '/*',
+    checkPublicApiVersionHeaders,
+    initializeMetrics,
+    wrapAsync(getPaymentInfo),
+    sendSuccess,
+  )
+
+  // Error handling middleware (needs to be defined last)
+  .use(errorHandler)
+
+export default publicApiRouter
