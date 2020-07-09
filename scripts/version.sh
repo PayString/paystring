abd9352580fe2d8c96c808037179cd17b9464485
Dino Rodriguez <dinoorodriguez@gmail.com>
2020-07-09 13:43:40 -0700

    fix tags

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,28 +1,6 @@
 #!/bin/sh
 
-# Bumps the NPM version, create a git tag.
-#
-# $1 - The current NPM version.
-# $2 - The type of version bump (major, minor, patch).
-function bump() {
-   declare -r current_version=$1
-   declare -r bump_increment=$2
-
-   if [[ $bump_increment != "major" && \
-         $bump_increment != "minor" && \
-         $bump_increment != "patch"
-   ]]; then
-      error "Invalid bump increment. Please specify 'major', 'minor', or 'patch'."
-   else
-      # Make sure versions are equal
-      compare_versions $current_version
-
-      # Bump NPM version and git tag
-      declare -r new_version=$(npm version $bump_increment)
-   fi
-}
-
-# Compares the NPM version, and Git tag version
+# Compares the Docker image version, NPM version, and Git tag version
 # in the repo. Throws an error if any do not match.
 #
 # $1 - The NPM version string.
