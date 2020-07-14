263685e9952c9c6da1b0190061c8290304f00ed8
Dino Rodriguez <dinoorodriguez@gmail.com>
2020-07-14 17:27:14 -0400

    refactor: bash script into lib (#571)
    * chore: script to compare versions
    
    * Update version.sh
    
    Co-authored-by: Hans Bergren <hansbergren@gmail.com>
    
    * nit: add blackmagic comment
    
    * chore+fix+feat: add compareVersions to lint, & fix outdated versions
    
    * no-op
    
    * 1.1.0
    
    * no-op
    
    * refactor: move script & turn into lib
    
    * assorted fixes
    
    * chore: version bump script (#572)
    
    * chore: add bump script
    
    * feat: initial bump script
    
    * 1.1.0
    
    * working bump script
    
    * 1.0.2
    
    * testing
    
    * 1.0.2
    
    * 1.1.0
    
    * 1.0.2
    
    * add updated docker compose
    
    * fix: drop versions
    
    * fix: drop parsing
    
    * fix tags
    
    * test bash
    
    * test
    
    * noop
    
    * workwork
    
    * workwork
    
    * swap . for source
    
    * make sure we are in a shell
    
    * workwork
    
    * workwork
    
    * drop comment
    
    * noop
    
    * fixes
    
    Co-authored-by: Hans Bergren <hansbergren@gmail.com>
    Co-authored-by: Tyler Longwell <tlongwell@ripple.com>

---
+++ scripts/version.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Compares the Docker image version, NPM version, and Git tag version
+# in the repo. Throws an error if any do not match.
+#
+# $1 - The NPM version string.
+function compare_versions {
+   declare -r npm_version=$1
+   declare -r git_tag_version=$(git describe --tags | cut -f 1 -d '-')
+
+   # Drop the 'v' from the Git tag for the comparison.
+   if [[ $npm_version != ${git_tag_version:1} ]]; then
+      error "Version Mismatch:
+
+      NPM Version: "$npm_version"
+      Git Tag Version: "${git_tag_version:1}"
+      "
+   else
+      echo "NPM, Git Tag Version: "$npm_version
+   fi
+}
+
+# Prints an error string to stderr, and exits the program with a 1 code.
+#
+# $1 - The error string to print.
+function error {
+	echo "ERROR - $1" 1>&2
+	exit 1
+}
