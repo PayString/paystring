fe90976cdc68a76c972f93111a80ac5a8b5ee435
nkramer44 <noah.ph.kramer@gmail.com>
2020-07-16 18:39:33 -0400

    fix jsdoc to make the linter love me

--- src/middlewares/jrdLinks.ts
+++ src/middlewares/jrdLinks.ts
@@ -9,6 +9,8 @@ interface JrdLink {
 }
 
 /**
+ * Generate a list of JrdLinks to return as a result of PayID Discovery.
+ *
  * @returns An array of JrdLinks containing PayID Discovery metadata.
  */
 export default function generateLinks(): JrdLink[] {
