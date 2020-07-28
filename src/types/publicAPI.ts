3c05e069a11d0387f353f713211108189b445abb
Austin King <aking@ripple.com>
2020-07-28 16:26:39 -0400

    feat: add VerifiedAddress type into types directory

--- src/types/publicAPI.ts
+++ src/types/publicAPI.ts
@@ -35,7 +35,7 @@ export interface PaymentInformation {
 /**
  * Address information included inside of a PaymentInformation object.
  */
-interface Address {
+export interface Address {
   readonly paymentNetwork: string
   readonly environment?: string
   readonly addressDetailsType: AddressDetailsType
---
+++ src/types/verifiedAddress.ts
@@ -0,0 +1,36 @@
+import { Address } from './publicAPI'
+
+/**
+ * Object containing address information alongside signatures.
+ */
+// eslint-disable-next-line import/no-unused-modules -- will disable in next PR
+export interface VerifiedAddress {
+  readonly payload: VerifiedAddressPayload
+  readonly signatures: VerifiedAddressSignature[]
+}
+
+interface VerifiedAddressPayload {
+  payId: string
+  payIdAddress: Address
+}
+
+/**
+ * Object containing a signature corresponding to an address.
+ *
+ * Protected -- base64 encoded identity key
+ * signature -- base64 encoded signatureA.
+ */
+interface VerifiedAddressSignature {
+  name: VerifiedAddressSignatureType
+  protected: string
+  signature: string
+}
+
+/**
+ * Identifies the type of signature being used, as of now we are only
+ * implementing IdentityKey signatures, but future types like
+ * WebPKI or ProofOfControl should be added to this enum.
+ */
+enum VerifiedAddressSignatureType {
+  IdentityKey = 'IdentityKey',
+}
