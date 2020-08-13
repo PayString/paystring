76323f947898017769e5487f773e5fe4d751395e
Dino Rodriguez <dinoorodriguez@gmail.com>
2020-08-12 20:14:54 -0400

    refactor: constants

--- test/integration/e2e/public-api/basePayIdContentNegotiation.test.ts
+++ test/integration/e2e/public-api/basePayIdContentNegotiation.test.ts
@@ -9,8 +9,38 @@ import { appSetup, appCleanup } from '../../../helpers/helpers'
 
 let app: App
 
+const PAYID = '/alice'
 const XRPL_TESTNET_ACCEPT_HEADER = 'application/xrpl-testnet+json'
 const XRPL_MAINNET_ACCEPT_HEADER = 'application/xrpl-mainnet+json'
+const XRPL_EXPECTED_TESTNET_RESPONSE = {
+  addresses: [
+    {
+      paymentNetwork: 'XRPL',
+      environment: 'TESTNET',
+      addressDetailsType: 'CryptoAddressDetails',
+      addressDetails: {
+        address: 'rDk7FQvkQxQQNGTtfM2Fr66s7Nm3k87vdS',
+      },
+    },
+  ],
+  verifiedAddresses: [],
+  payId: 'alice$127.0.0.1',
+}
+const XRPL_EXPECTED_MAINNET_RESPONSE = {
+  addresses: [
+    {
+      paymentNetwork: 'XRPL',
+      environment: 'MAINNET',
+      addressDetailsType: 'CryptoAddressDetails',
+      addressDetails: {
+        address: 'rw2ciyaNshpHe7bCHo4bRWq6pqqynnWKQg',
+        tag: '67298042',
+      },
+    },
+  ],
+  verifiedAddresses: [],
+  payId: 'alice$127.0.0.1',
+}
 
 describe('E2E - publicAPIRouter - Content Negotiation', function (): void {
   // Boot up Express application and initialize DB connection pool
@@ -20,27 +50,12 @@ describe('E2E - publicAPIRouter - Content Negotiation', function (): void {
 
   it('Returns the first address for multiple types with no q', function (done): void {
     // GIVEN a payment pointer known to have an associated xrpl-testnet and xrpl-mainnet address
-    const payId = '/alice'
     const acceptHeader = `${XRPL_TESTNET_ACCEPT_HEADER}, ${XRPL_MAINNET_ACCEPT_HEADER}`
-    const expectedResponse = {
-      addresses: [
-        {
-          paymentNetwork: 'XRPL',
-          environment: 'TESTNET',
-          addressDetailsType: 'CryptoAddressDetails',
-          addressDetails: {
-            address: 'rDk7FQvkQxQQNGTtfM2Fr66s7Nm3k87vdS',
-          },
-        },
-      ],
-      verifiedAddresses: [],
-      payId: 'alice$127.0.0.1',
-    }
 
     // WHEN we make a GET request to the public endpoint to retrieve payment info with an Accept header specifying
     // both testnet and mainnet, with no q for either
     request(app.publicApiExpress)
-      .get(payId)
+      .get(PAYID)
       .set('PayID-Version', '1.1')
       .set('Accept', acceptHeader)
       // THEN we get back an xrpl testnet header as our Content-Type
@@ -51,32 +66,17 @@ describe('E2E - publicAPIRouter - Content Negotiation', function (): void {
         )
       })
       // AND we get back the xrpl-testnet account information associated with that payment pointer for xrpl-testnet
-      .expect(HttpStatus.OK, expectedResponse, done)
+      .expect(HttpStatus.OK, XRPL_EXPECTED_TESTNET_RESPONSE, done)
   })
 
   it('Returns the preferred available address where the higher q is at the beginning', function (done): void {
     // GIVEN a payment pointer known to have an associated xrpl-testnet address and xrpl-mainnet address
-    const payId = '/alice'
     const acceptHeader = `${XRPL_TESTNET_ACCEPT_HEADER}; q=1.1, ${XRPL_MAINNET_ACCEPT_HEADER}; q=0.5`
-    const expectedResponse = {
-      addresses: [
-        {
-          paymentNetwork: 'XRPL',
-          environment: 'TESTNET',
-          addressDetailsType: 'CryptoAddressDetails',
-          addressDetails: {
-            address: 'rDk7FQvkQxQQNGTtfM2Fr66s7Nm3k87vdS',
-          },
-        },
-      ],
-      verifiedAddresses: [],
-      payId: 'alice$127.0.0.1',
-    }
 
     // WHEN we make a GET request to the public endpoint to retrieve payment info with an Accept header specifying testnet
     // and mainnet, with testnet having a higher q-value
     request(app.publicApiExpress)
-      .get(payId)
+      .get(PAYID)
       .set('PayID-Version', '1.1')
       .set('Accept', acceptHeader)
       // THEN we get back an xrpl testnet header as the Content-Type
@@ -87,33 +87,17 @@ describe('E2E - publicAPIRouter - Content Negotiation', function (): void {
         )
       })
       // AND we get back the xrpl testnet account information associated with that payment pointer for xrpl testnet
-      .expect(HttpStatus.OK, expectedResponse, done)
+      .expect(HttpStatus.OK, XRPL_EXPECTED_TESTNET_RESPONSE, done)
   })
 
   it('Returns the preferred available address where the higher q is at the end', function (done): void {
     // GIVEN a payment pointer known to have an associated xrpl-testnet address and an xrpl-mainnet address
-    const payId = '/alice'
     const acceptHeader = `${XRPL_TESTNET_ACCEPT_HEADER}; q=0.5, ${XRPL_MAINNET_ACCEPT_HEADER}; q=1.1`
-    const expectedResponse = {
-      addresses: [
-        {
-          paymentNetwork: 'XRPL',
-          environment: 'MAINNET',
-          addressDetailsType: 'CryptoAddressDetails',
-          addressDetails: {
-            address: 'rw2ciyaNshpHe7bCHo4bRWq6pqqynnWKQg',
-            tag: '67298042',
-          },
-        },
-      ],
-      verifiedAddresses: [],
-      payId: 'alice$127.0.0.1',
-    }
 
     // WHEN we make a GET request to the public endpoint to retrieve payment info with an Accept header specifying
     // xrpl-testnet and xrpl-mainnet, with a higher q for xrpl-mainnet
     request(app.publicApiExpress)
-      .get(payId)
+      .get(PAYID)
       .set('PayID-Version', '1.1')
       .set('Accept', acceptHeader)
       // THEN we get back a xrpl-mainnet accept header as the Content-Type
@@ -124,34 +108,18 @@ describe('E2E - publicAPIRouter - Content Negotiation', function (): void {
         )
       })
       // AND we get back the xrpl-mainnet account information associated with that payment pointer for xrpl-mainnet.
-      .expect(HttpStatus.OK, expectedResponse, done)
+      .expect(HttpStatus.OK, XRPL_EXPECTED_MAINNET_RESPONSE, done)
   })
 
   it('Returns a valid address when the most preferred type does not exist', function (done): void {
     // GIVEN a payment pointer known to have an associated xrpl-testnet address and mainnet address
-    const payId = '/alice'
     const nonExistentAcceptType = 'application/fakenetwork-fakenet+json'
     const acceptHeader = `${nonExistentAcceptType}; q=1.1, ${XRPL_TESTNET_ACCEPT_HEADER}; q=0.5, ${XRPL_MAINNET_ACCEPT_HEADER}; q=0.9`
-    const expectedResponse = {
-      addresses: [
-        {
-          paymentNetwork: 'XRPL',
-          environment: 'MAINNET',
-          addressDetailsType: 'CryptoAddressDetails',
-          addressDetails: {
-            address: 'rw2ciyaNshpHe7bCHo4bRWq6pqqynnWKQg',
-            tag: '67298042',
-          },
-        },
-      ],
-      verifiedAddresses: [],
-      payId: 'alice$127.0.0.1',
-    }
 
     // WHEN we make a GET request to the public endpoint to retrieve payment info with an Accept header specifying
     // a non-existent network+environment most preferred, followed by xrpl-mainnet and xrpl-testnet
     request(app.publicApiExpress)
-      .get(payId)
+      .get(PAYID)
       .set('PayID-Version', '1.1')
       .set('Accept', acceptHeader)
       // THEN we get back a xrpl-mainnet accept header as the Content-Type
@@ -162,7 +130,7 @@ describe('E2E - publicAPIRouter - Content Negotiation', function (): void {
         )
       })
       // AND we get back the xrpl-mainnet account information associated with that payment pointer for xrpl mainnet.
-      .expect(HttpStatus.OK, expectedResponse, done)
+      .expect(HttpStatus.OK, XRPL_EXPECTED_MAINNET_RESPONSE, done)
   })
 
   // Shut down Express application and close DB connections
